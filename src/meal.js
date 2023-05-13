075d151ba42a2148ceecae3799cbe969f044c1d8
Mary Bruff <marybruff5@gmail.com>
2023-05-13 16:58:36 -0600

    solve up to createRecipe test

--- src/meal.js
+++ src/meal.js
@@ -1,8 +1,9 @@
 function nameMenuItem(name) {
 return `Delicious ${name}`
 }
+//string interpolation
 
-
+//putting into object
 function createMenuItem(name, price, type) {
   return {
     name: name,
@@ -13,19 +14,42 @@ function createMenuItem(name, price, type) {
 }
 
 
-function addIngredients (addIngredients) {
-  return 
+function addIngredients (newIngredients, ingredients) {
+  if (ingredients.includes(newIngredients)) {
+    return ingredients 
+  } else { 
+    return ingredients.push(newIngredients);
+  }  
 }
 
+function formatPrice (initialPrice) {
+  return `$${initialPrice}`
+}
+
+function decreasePrice (price) {
+
+var percentDecrease = (price * 10) / 100 
+
+return price - percentDecrease
+
+}
+
+ 
+
+// the problem I want to solve is decreasing the price by 10%
+// I want to access the menu item and the price I need to decrease which is 6.00
+// I am going to get 10 percent of 6.00
+// what is 10 percent of 6 and then calculate that by subtracting 100
+// I want to return decreased price
 
 
 module.exports = {
   nameMenuItem,
   createMenuItem,
   addIngredients,
-  // formatPrice,
-  // decreasePrice,
+  formatPrice,
+  decreasePrice,
   // createRecipe
 }
 
-
+//names of functions under module.exports//
