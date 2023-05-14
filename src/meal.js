d9803ad92a129b3f0d273f1bafe1e3adebeaa13f
Mary Bruff <marybruff5@gmail.com>
2023-05-14 10:42:21 -0600

    finish passing all the functions in the meals test

--- src/meal.js
+++ src/meal.js
@@ -34,14 +34,21 @@ return price - percentDecrease
 
 }
 
- 
-
 // the problem I want to solve is decreasing the price by 10%
 // I want to access the menu item and the price I need to decrease which is 6.00
 // I am going to get 10 percent of 6.00
 // what is 10 percent of 6 and then calculate that by subtracting 100
 // I want to return decreased price
 
+/////////////////////////////////////////////////////////  
+ 
+function createRecipe(title, ingredients , type ) {
+  return {
+    title: title,
+    ingredients: ingredients,
+    type: type,
+  } 
+}
 
 module.exports = {
   nameMenuItem,
@@ -49,7 +56,7 @@ module.exports = {
   addIngredients,
   formatPrice,
   decreasePrice,
-  // createRecipe
+  createRecipe
 }
 
 //names of functions under module.exports//
