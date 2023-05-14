5fef310d50d2403805d6b191aa7d121222cce1f5
Mary Bruff <marybruff5@gmail.com>
2023-05-14 15:36:04 -0600

    add solution to first function

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,8 +1,35 @@
+function createRestaurant (name) {
+  var pizzaRestaurant = {
+    name : name,
+    menus: {
+      breakfast : [],
+      lunch: [],
+      dinner: [],
+
+    }
+  }
+  return pizzaRestaurant
+}
+
+function addMenuItem (restaurantName, itemName) {
+  restaurantName.menus.lunch.push(itemName)
+  return
+}
+
+
+// assert statement comparing-- comparing the expected number and message
+// how many arguments-- 
+// data type
+
+
+
+
+
 
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
+  createRestaurant, 
+  addMenuItem,
   // removeMenuItem,
   // checkForFood
 }
